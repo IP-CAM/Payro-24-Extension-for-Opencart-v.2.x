392e61c0b256dd63c89b67a2560b9ca28c07e87b
matin <matinphonej7@gmail.com>
2021-07-18 11:33:32 +0430

    First commit

---
+++ catalog/model/payment/payro24.php
@@ -0,0 +1,32 @@
+<?php 
+
+class ModelPaymentpayro24 extends Model
+{
+	public function getMethod($address)
+	{
+		$this->load->language('payment/payro24');
+
+		if ($this->config->get('payro24_status')) {
+
+			$status = true;
+
+		} else {
+
+			$status = false;
+		}
+
+		$method_data = array ();
+
+		if ($status) {
+
+			$method_data = array (
+        		'code'       => 'payro24',
+        		'title'      => $this->language->get('text_title'),
+				'terms'      => null,
+				'sort_order' => $this->config->get('payro24_sort_order')
+			);
+		}
+
+		return $method_data;
+	}
+}
